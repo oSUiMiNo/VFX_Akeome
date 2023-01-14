e25d02accfae8448eb1eff53e87956e449a884a7
大平 周生 <t170d025@gunma-u.ac.jp>
2023-01-14 21:00:35 +0900

    a

--- Assets/Scripts/MeshParticle.cs
+++ Assets/Scripts/MeshParticle.cs
@@ -6,19 +6,11 @@ using UnityEngine;
 [ExecuteAlways]
 public class MeshParticle : MonoBehaviour
 {
-    //public static MeshParticleUnit unitPrefab1;
-    //public static MeshParticleUnit unitPrefab2;
-
-    //[SerializeField] private float effectDiableDelay = 10f;
-    //[SerializeField] private float pointCountPerArea = 10000f;
-   
-    //public bool startEffect;
-    
     public GameObject model;
-    private float modelDiableDelay = 2;
-    private float modelEnableDelay = 6.5f;
-    private float animEnableDelay = 1;
-    private float autoDestroyDelay = 22;
+    float modelDiableDelay = 2;
+    float modelEnableDelay = 6.5f;
+    float animEnableDelay = 1;
+    float autoDestroyDelay = 22;
 
     class Eff
     {
@@ -31,7 +23,7 @@ public class MeshParticle : MonoBehaviour
             string unitPrefabName,
             float effectDiableDelay,
             float pointCountPerArea,
-            bool activeness = true
+            bool startEffect = true
             )
         {
             GameObject a = (GameObject)Resources.Load(unitPrefabName);
@@ -39,76 +31,33 @@ public class MeshParticle : MonoBehaviour
             this.unit = Instantiate(unitPrefab);
             this.effectDiableDelay = effectDiableDelay;
             this.pointCountPerArea = pointCountPerArea;
-            this.startEffect = activeness;
+            this.startEffect = startEffect;
 
             Debug.Log($"Effコンストラクタ {unit}");
         }
     }
-
-    //[SerializeField] List<MeshParticleUnit> units = new List<MeshParticleUnit>();
-    //[SerializeField] Dictionary<MeshParticleUnit, float> units = new Dictionary<MeshParticleUnit, float>();
-    [SerializeField] List<Eff> effs = new List<Eff>();
+    List<Eff> effs = new List<Eff>();
     
     Animator pausedAnim;
 
-    private void Start()
+    void Start()
     {
-        effs.Add(new Eff("MeshParticleVFXUnit1", modelEnableDelay + 5,6000));
+        effs.Add(new Eff("MeshParticleVFXUnit1", modelEnableDelay + 5, 6000));
         effs.Add(new Eff("MeshParticleVFXUnit2", modelDiableDelay, 3000));
         effs.Add(new Eff("MeshParticleVFXUnit3", modelEnableDelay + 15, 3000));
-        Debug.Log(modelDiableDelay);
-        //LoadResources();
         Destroy(gameObject, autoDestroyDelay);
         StartCoroutine(StartTargetActiveSequence());
-        effs.ForEach(eff => Debug.Log(eff));
         effs.ForEach(eff => UpdateEffect(eff));
     }
 
-    //void LoadResources()
-    //{
-    //    if (unitPrefab1 == null)
-    //    {
-    //        GameObject a = (GameObject)Resources.Load("MeshParticleVFXUnit1");
-    //        unitPrefab1 = a.GetComponent<MeshParticleUnit>();
-    //    }
-    //    if (unitPrefab2 == null)
-    //    {
-    //        GameObject a = (GameObject)Resources.Load("MeshParticleVFXUnit2");
-    //        unitPrefab2 = a.GetComponent<MeshParticleUnit>();
-    //    }
-    //}
 
-    //private void Update()
-    //{
-    //    if (effs.Any())
-    //    {
-    //        Debug.Log("通った1");
-    //        effs.ForEach(eff => UpdateEffect(eff));
-    //    }
-    //}
-
-    private void UpdateEffect(Eff eff)
+    void UpdateEffect(Eff eff)
     {
         if (!eff.startEffect) return;
-        Debug.Log("通った2");
         InitEffect(eff);
-
         StartCoroutine(StartEffectAtiveSequence(eff));
     }
-    //private void UpdateEffect(Eff eff)
-    //{
-    //    if (startEffect && units.Any() == false)
-    //    {
-    //        InitEffect();
 
-    //        foreach (var a in units)
-    //        {
-    //            StartCoroutine(StartEffectAtiveSequence(a.Key, a.Value));
-    //        }
-    //        StartCoroutine(StartTargetActiveSequence());
-    //        Destroy(gameObject, autoDestroyDelay);
-    //    }
-    //}
 
     IEnumerator StartTargetActiveSequence()
     {
@@ -134,13 +83,11 @@ public class MeshParticle : MonoBehaviour
         yield return new WaitForSeconds(eff.effectDiableDelay);
         Debug.Log($"2  {eff.unit}  {eff.effectDiableDelay}");
         Destroy(eff.unit.gameObject);
-        effs.Remove(eff);
-        
-        //startEffect = false;
+        effs.Remove(eff);        
     }
 
 
-    private void InitEffect(Eff eff)
+    void InitEffect(Eff eff)
     {
 
         Debug.Log("Init");
@@ -151,24 +98,12 @@ public class MeshParticle : MonoBehaviour
         foreach (var (mesh, tex) in meshAndTexs)
         {
             Debug.Log("Init2");
-            //units.Add(Instantiate(eff.unit));
-            //foreach (var a in units)
-            //{
-            //    a.gameObject.SetActive(true);
-            //    a.transform.SetParent(transform, false);
-
-            //    a.mapSet = MeshToMap.ComputeMap(mesh, pointCountPerArea);
-            //    a.modelMainTex = tex;
-            //}
-
+        
             eff.unit.gameObject.SetActive(true);
             eff.unit.transform.SetParent(transform, false);
 
             eff.unit.mapSet = MeshToMap.ComputeMap(mesh, eff.pointCountPerArea);
             eff.unit.modelMainTex = tex;
-
-
-            //unit.effect.SetFloat("ParticleSize", particleSize);
         }
     }
 
